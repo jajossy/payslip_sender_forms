d8f8dc9d2c4390950dfc533bd624d0c30a672a85
Jolaosho Joseph <jajossy@hotmail.com>
2019-07-29 12:35:49 +0100

    second Commit

--- payslip_sender_forms/Program.cs
+++ payslip_sender_forms/Program.cs
@@ -16,7 +16,8 @@ namespace payslip_sender_forms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            Application.Run(new MDIParent1());
         }
     }
 }
